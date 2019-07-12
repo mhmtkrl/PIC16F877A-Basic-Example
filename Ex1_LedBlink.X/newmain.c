9052c09204fdaedc8a48b070caab7f22754014ce
Mehmet KORAL <43435126+mhmtkrl@users.noreply.github.com>
2019-07-13 01:37:22 +0300

    Led blink on RD0

--- Ex1_LedBlink.X/newmain.c
+++ Ex1_LedBlink.X/newmain.c
@@ -7,7 +7,7 @@
 
 
 #include <xc.h>
-#include "fuses.h"                  //Sigorta ayarlarý programa dahil edildi
+#include "fuses.h"                  //Settings of Fuse
 #define _XTAL_FREQ 20000000
 
 #define led PORTDbits.RD0           //RD0 is an output pin that we'will use for blinking to led 
