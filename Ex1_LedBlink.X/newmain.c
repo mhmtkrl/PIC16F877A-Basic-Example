c50c4136a446171d8ab1eeb8335a2db147020703
Mehmet KORAL <43435126+mhmtkrl@users.noreply.github.com>
2019-07-13 01:34:42 +0300

    Led blink

---
+++ Ex1_LedBlink.X/newmain.c
@@ -0,0 +1,26 @@
+/*
+ * File:   newmain.c
+ * Author: Mehmet KORAL
+ *
+ * Created on 21.01.2017 Saturday, 16:34
+ */
+
+
+#include <xc.h>
+#include "fuses.h"                  //Sigorta ayarlarý programa dahil edildi
+#define _XTAL_FREQ 20000000
+
+#define led PORTDbits.RD0           //RD0 is an output pin that we'will use for blinking to led 
+
+void main(void) {
+    TRISD = 0x00;                   //PORTD is set as an output
+    PORTD = 0x00;                   //Clear data on PORTD
+    
+    while(1) {
+        led = 1;                    //RD0 is high
+        __delay_ms(100);
+        led = 0;                    //RD0 is low
+        __delay_ms(100);
+    }
+    return;
+}
